67e2c6e74a1bd88eae21ccea1384b9025589348d
Alexandra-Jev <32392524+Alexandra-Jev@users.noreply.github.com>
2018-04-09 11:01:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    console.log( "Yes I use Jquery" );
+
+    $('#invertedmode').click(function(){
+      $('#bestmotherfuckingWebsite').addClass('inverted');
+    });
+    $('#invertedmodeOff').click(function(){
+      $('#bestmotherfuckingWebsite').removeClass('inverted');
+    })
+});
